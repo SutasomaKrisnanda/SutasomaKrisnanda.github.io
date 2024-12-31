c368e60b6bb95a526a528d36aeca64628a1a3b0e
sutasomakrisnanda <sutasomakrisnanda@gmail.com>
2024-12-31 18:19:08 +0000

    Update

--- SutasomaKrisnanda/src/main.jsx
+++ SutasomaKrisnanda/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<div>Hello, Page Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
