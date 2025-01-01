e044ea4d2c97fd0ed4039419012efc43f6b5818f
sutasomakrisnanda <sutasomakrisnanda@gmail.com>
2025-01-01 13:35:48 +0000

    Adding TailwindCSS

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
+// import App from './App'
+import HomePage from './pages/HomePage/HomePage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/SutasomaKrisnanda.github.io">
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/Hello" element={<div>Hello, Page Not Found</div>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hello" element={<div>Hello, Page Not Found</div>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
