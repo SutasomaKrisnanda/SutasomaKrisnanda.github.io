53b2a979e9b91b787cc8c37c8c4ff8104525cb15
sutasomakrisnanda <sutasomakrisnanda@gmail.com>
2025-01-01 07:48:42 +0000

    Add Routing in React.

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<div>Hello, Page Not Found</div>} />
+        <Route path="/" element={<App />} />
+        <Route path="/Hello" element={<div>Hello, Page Not Found</div>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
